3e9ecf93b0bc68d600a74cf1ea4fb2a8611f4196
arseneyr <arseneyr@users.noreply.github.com>
2020-10-01 15:12:37 -0700

    fixing map play screen

--- src/js/content/ggShims/MapPlay.tsx
+++ src/js/content/ggShims/MapPlay.tsx
@@ -13,19 +13,35 @@ function select(el: Element) {
   el.classList.add("radio-box--selected");
 }
 
+enum Selectors {
+  RadioBoxes = "div.radio-boxes",
+  ChallengeIdCopyBox = 'input[name="copy-link"]',
+  Slider = "div.rangeslider.rangeslider-horizontal[aria-valuenow]",
+  GameSettings = "article.game-settings",
+  GameButton = "div.game-settings__section > button",
+  RadioBoxLabel = "div.radio-box__label",
+  SelectedRadioBox = "div.radio-box.radio-box--selected",
+  SettingsCheckbox = 'input[type="checkbox"]',
+  TimeLimitText = "div.game-settings__detailed-settings > div:first-child",
+}
+
 const useStyles = makeStyles({
   hideOriginal: {
-    "& div.game-settings > button:first-of-type, & div.game-settings__section > :nth-child(3)": {
+    [`& > div.game-settings__section > button:first-child, 
+    & > div.game-settings__section > div > div.game-settings__checkbox > :first-child,
+    & div.game-settings__detailed-settings > div:first-child`]: {
       display: "none !important",
     },
   },
   hideNew: {
-    "& div.game-settings > button:last-of-type, & div.game-settings__section > :nth-child(4)": {
+    [`& > div.game-settings__section > button:last-child, 
+    & div.game-settings__section:nth-child(2) > :nth-child(2),
+    & div.game-settings__detailed-settings > div:nth-child(2)`]: {
       display: "none !important",
     },
   },
   hideAllSettings: {
-    "& > div.game-settings": {
+    "& > div.game-settings__section": {
       display: "none !important",
     },
   },
@@ -36,10 +52,7 @@ const useStyles = makeStyles({
 
 const useSliderMonitor = (gameSettings: Element | null) => {
   const [timeLimit, setTimeLimit] = useState<number | null>(null);
-  const slider = useExternalDom(
-    gameSettings,
-    "div.rangeslider.rangeslider-horizontal[aria-valuenow]"
-  );
+  const slider = useExternalDom<HTMLDivElement>(gameSettings, Selectors.Slider);
   const sliderMo = useRef<MutationObserver>(
     new MutationObserver((changes) =>
       changes.forEach(({ target }) => {
@@ -68,10 +81,10 @@ const useSliderMonitor = (gameSettings: Element | null) => {
 
 const useChallengeIdMonitor = (gameSettings: HTMLDivElement | null) => {
   const [challengeId, setChallengeId] = useState<string | null>(null);
-  const input = useExternalDom(
+  const input = useExternalDom<HTMLInputElement>(
     gameSettings,
-    "input[name=copy-link]"
-  ) as HTMLInputElement | null;
+    Selectors.ChallengeIdCopyBox
+  );
   useEffect(() => {
     if (input) {
       const value = input.value;
@@ -91,31 +104,34 @@ const useChallengeIdMonitor = (gameSettings: HTMLDivElement | null) => {
 const MapPlayShimComponent = () => {
   const dispatch = useAppDispatch();
   const classes = useStyles();
-  const gameSettings = useExternalDom(
+  const gameSettings = useExternalDom<HTMLDivElement>(
     document,
-    "article.game-settings",
+    Selectors.GameSettings,
     true
-  ) as HTMLDivElement | null;
+  );
   const timeLimit = useSliderMonitor(gameSettings);
   const [startLobby, setStartLobby] = useState(false);
   const selected = useRef(false);
   const challengeId = useChallengeIdMonitor(startLobby ? gameSettings : null);
   const newButton = useRef<HTMLButtonElement | null>(null);
+  const timeLimitText = useExternalDom<HTMLDivElement>(
+    gameSettings,
+    Selectors.TimeLimitText
+  );
 
   useEffect(() => {
-    const radioBoxes = gameSettings?.querySelector("div.radio-boxes");
+    const radioBoxes = gameSettings?.querySelector(Selectors.RadioBoxes);
     if (gameSettings && radioBoxes) {
       const challengeBox = radioBoxes.children[1];
 
       const newBox = challengeBox.cloneNode(true) as HTMLDivElement;
       unselect(newBox);
-      (newBox.querySelector(
-        "div.radio-box__label"
-      ) as HTMLDivElement).innerText = "Play With Lobby";
+      newBox.querySelector<HTMLDivElement>(Selectors.RadioBoxLabel)!.innerText =
+        "Play With Lobby";
 
-      let oldButton = document.querySelector(
-        "div.game-settings > button"
-      )! as HTMLButtonElement;
+      let oldButton = document.querySelector<HTMLButtonElement>(
+        Selectors.GameButton
+      )!;
       newButton.current = oldButton.cloneNode(true) as HTMLButtonElement;
       newButton.current.addEventListener("click", () => {
         if (newButton.current?.classList.contains("button--disabled")) {
@@ -131,48 +147,87 @@ const MapPlayShimComponent = () => {
 
       gameSettings.classList.add(classes.hideNew);
 
+      let performSelection: (() => void) | null = null;
+
       // class attribute modification happens asynchronously
-      const mo = new MutationObserver((changes) =>
-        changes.forEach(({ target }) => {
-          if (
-            target instanceof Element &&
-            target.className.includes("radio-box--selected") &&
-            selected.current
-          ) {
-            unselect(target);
+      const mo = new MutationObserver((changes) => {
+        const oldButton = gameSettings.querySelector(Selectors.GameButton);
+        if (
+          changes.some(({ addedNodes }) =>
+            Array.from(addedNodes).some((n) => n.contains(oldButton))
+          )
+        ) {
+          if (performSelection) {
+            performSelection();
+            performSelection = null;
           }
-        })
-      );
+          oldButton?.insertAdjacentElement("afterend", newButton.current!);
+        }
+      });
+      mo.observe(gameSettings, { childList: true, subtree: true });
       for (const i of radioBoxes.children) {
-        mo.observe(i, { attributes: true, attributeFilter: ["class"] });
         // eslint-disable-next-line no-loop-func
-        i.addEventListener("click", ({ target }) => {
+        i.addEventListener("click", () => {
           if (selected.current) {
-            unselect(newBox);
-            select(i);
             selected.current = false;
-            gameSettings.classList.remove(classes.hideOriginal);
-            gameSettings.classList.add(classes.hideNew);
+            if (i === challengeBox) {
+              unselect(newBox);
+              select(challengeBox);
+              gameSettings.classList.remove(classes.hideOriginal);
+              gameSettings.classList.add(classes.hideNew);
+              gameSettings
+                .querySelector(Selectors.SettingsCheckbox + ":not(:checked)")
+                ?.dispatchEvent(new Event("change", { bubbles: true }));
+            } else {
+              performSelection = () => {
+                unselect(newBox);
+                select(i);
+                gameSettings.classList.remove(classes.hideOriginal);
+                gameSettings.classList.add(classes.hideNew);
+              };
+            }
           }
         });
       }
-      newBox.addEventListener("click", () => {
-        selected.current = true;
-        challengeBox.children[0].children[0].dispatchEvent(
-          new Event("change", { bubbles: true, cancelable: true })
-        );
-        unselect(challengeBox);
-        select(newBox);
-        gameSettings.classList.add(classes.hideOriginal);
-        gameSettings.classList.remove(classes.hideNew);
+      newBox.addEventListener("click", (e) => {
+        e.preventDefault();
+        if (!selected.current) {
+          selected.current = true;
+          challengeBox.children[0].children[0].dispatchEvent(
+            new Event("change", { bubbles: true, cancelable: true })
+          );
+          const currentSelection = gameSettings.querySelector<HTMLDivElement>(
+            Selectors.SelectedRadioBox
+          )!;
+          if (currentSelection === challengeBox) {
+            unselect(challengeBox);
+            select(newBox);
+            gameSettings.classList.add(classes.hideOriginal);
+            gameSettings.classList.remove(classes.hideNew);
+            gameSettings
+              .querySelector(Selectors.SettingsCheckbox + ":checked")
+              ?.dispatchEvent(new Event("change", { bubbles: true }));
+          } else {
+            performSelection = () => {
+              unselect(currentSelection);
+              unselect(challengeBox);
+              select(newBox);
+              gameSettings.classList.add(classes.hideOriginal);
+              gameSettings.classList.remove(classes.hideNew);
+              gameSettings
+                .querySelector(Selectors.SettingsCheckbox + ":checked")
+                ?.dispatchEvent(new Event("change", { bubbles: true }));
+            };
+          }
+        }
       });
 
       radioBoxes.appendChild(newBox);
 
       return () => {
         mo.disconnect();
-        newButton.current?.parentNode?.removeChild(newButton.current);
-        newBox.parentNode?.removeChild(newBox);
+        newButton.current?.remove();
+        newBox.remove();
         gameSettings.classList.remove(
           classes.hideOriginal,
           classes.hideNew,
@@ -203,31 +258,38 @@ const MapPlayShimComponent = () => {
     }
   }, [gameSettings, startLobby, classes]);
 
-  const newLabel = useMemo(() => {
+  const [
+    newLabelContainer,
+    setNewLabelContainer,
+  ] = useState<HTMLDivElement | null>(null);
+  useEffect(() => {
+    if (timeLimitText) {
+      const newLabel = timeLimitText.cloneNode() as HTMLDivElement;
+      timeLimitText.insertAdjacentElement("afterend", newLabel);
+      setNewLabelContainer(newLabel);
+      return () => {
+        setNewLabelContainer(null);
+      };
+    }
+  }, [timeLimitText]);
+
+  const newLabelText = useMemo(() => {
     const invalidTimeLimit =
       timeLimit === null || timeLimit === 0 || timeLimit === 610;
-    return (
-      gameSettings && (
-        <div
-          className={`margin--top-small ${
-            invalidTimeLimit ? classes.redLabel : ""
-          }`}
-        >
-          {invalidTimeLimit
-            ? "Please set a time limit"
-            : (gameSettings.querySelector(
-                "div.game-settings__section > :nth-child(3)"
-              ) as HTMLElement).innerText}
-        </div>
-      )
-    );
-  }, [gameSettings, classes, timeLimit]);
+    if (newLabelContainer) {
+      if (invalidTimeLimit) {
+        newLabelContainer.classList.add(classes.redLabel);
+      } else {
+        newLabelContainer.classList.remove(classes.redLabel);
+      }
+    }
+    return invalidTimeLimit || !timeLimitText?.innerText
+      ? "Please set a time limit"
+      : timeLimitText.innerText;
+  }, [classes, timeLimit, timeLimitText, newLabelContainer]);
 
-  return !challengeId && gameSettings
-    ? createPortal(
-        newLabel,
-        gameSettings.querySelector("div.game-settings__section")!
-      )
+  return !challengeId && gameSettings && newLabelContainer
+    ? createPortal(newLabelText, newLabelContainer)
     : null;
 };
 
